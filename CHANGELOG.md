9bbe7a2c402a584c8d4b34c5a88af77aa0b3930a
wangyiming <wangyiming.fe@bytedance.com>
2025-04-09 14:36:23 +0800

    fix: default value of type filed should commonjs

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# ndepe
+
+## 0.1.8
+
+### Patch Changes
+
+- fix: default value of type filed should commonjs
+- fix: type 字段的默认值应该是 commonjs
--- src/index.ts
+++ src/index.ts
@@ -338,7 +338,7 @@ export const nodeDepEmit = async ({
 		name: `${projectPkgJson.name || "modernjs-project"}-prod`,
 		version: projectPkgJson.version || "0.0.0",
 		private: true,
-		type: projectPkgJson.type || "module",
+		type: projectPkgJson.type || "commonjs",
 		dependencies: Object.fromEntries(
 			[
 				...Object.values(tracedPackages).map((pkg) => [
